1b12e1379449e3c71516d8273226ce62968b7365
Nirmal Mekala <ndmekala@icloud.com>
2026-04-14 11:04:48 -0500

    static and dynamic routes with color component

---
+++ src/app/dynamic/page.tsx
@@ -0,0 +1,7 @@
+import ColorElement from "../components/ColorElement";
+
+export const dynamic = "force-dynamic";
+
+export default function Dynamic() {
+  return <ColorElement>Dynamic</ColorElement>;
+}
---
+++ src/app/static/page.tsx
@@ -0,0 +1,7 @@
+import ColorElement from "../components/ColorElement";
+
+export const dynamic = "force-static";
+
+export default function Static() {
+  return <ColorElement>Static</ColorElement>;
+}
